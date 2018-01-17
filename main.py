99d3eb3d829075c4bb5282c2b48e381e964a9441
Shane Galvin <sgalvinnc@gmail.com>
2018-01-17 02:48:35 -0500

    just basic, basic framework so far

---
+++ main.py
@@ -0,0 +1,209 @@
+import numpy as np
+from matplotlib import use
+use('tkagg') # select pyplot backend
+import matplotlib.pyplot as plt
+import math
+
+class SingleOutputModel():
+    def __init__(self):
+        self.x_shape = None
+        self.name = 'model'
+
+    def check_x_shape(self, x):
+        if self.x_shape != None:
+            t = repr(type(x))
+            if t!="<class 'numpy.ndarray'>" and t!="<class 'numpy.core.memmap.memmap'>":
+                x = np.array(x)
+
+            if len(x.shape) != len(self.x_shape):
+                raise InvalidInputDimensions("Mismatch between {} input and {} expected".format(
+                    x.shape, self.x_shape
+                ))
+
+            for ex, rx in zip(self.x_shape, x.shape):
+                if ex == -1:
+                    continue
+                else:
+                    if ex != rx:
+                        raise InvalidInputDimensions("Mismatch between {} input and {} expected".format(
+                            x.shape, self.x_shape
+                        ))
+
+    def eval(self, x):
+        pass
+        # this function is defined by the model instance
+
+    def run(self, x):
+        self.check_x_shape(x)
+        return self.eval(x)
+
+    def run_multi(self, xs):
+        return [self.run(x) for x in xs]
+
+    def analysis(self, xs, ys, graph_path=None, csv_path=None):
+        pass
+
+    def calculate_one_one_error(self, actual_list, predicted_list):
+        '''
+        Compare both to (1,1)
+        Return dictionary with:
+        rms, tp, tn, fp, fn, e, l, q,
+        tpr, tnr, fpr, fnr, er, lr,
+        tprp, tprp, fprp, fnrp, erp, lrp,
+        (percentages rounded to 3 places)
+        c, cedb
+
+
+        with x:actual, y:predicted
+                    |
+          false     |  true
+          positive  |  positive
+        ------------+-------------
+          true      |  false
+          negative  |  negative
+                    |
+
+
+                            /
+                excessive /
+                        /
+                      /
+                    /
+                  /   lacking
+                /
+        '''
+        rms = 0 # root mean squared error
+
+        tp = 0 # true positive
+        tn = 0 # true negative
+        fp = 0 # false positive
+        fn = 0 # false negative
+
+        e = 0 # excessive (overestimate)
+        l = 0 # lacking (underestimate)
+
+        q = 0 # quantity
+
+        for z in zip(actual_list, predicted_list):
+            if z[0] >= 1:
+                # actual value is positive
+                if z[1] >= 1:
+                    # predicted is positive.
+                    # true positive
+                    tp += 1
+                else:
+                    # predicted is negative
+                    # false negative
+                    fn += 1
+            else:
+                # actual value is negative
+                if z[1] >= 1:
+                    # predicted is positive
+                    # false positive
+                    fp += 1
+                else:
+                    # predicted is negative
+                    # true negative
+                    tn += 1
+
+            if z[1] > z[0]:
+                # predicted is too high
+                # excessive
+                e += 1
+            elif z[1] < z[0]:
+                # predicted is too low
+                # lacking
+                l += 1
+
+            q += 1
+
+            rms += (z[0] - z[1])**2
+
+        if q == 0:
+            print("No values for error calculation!")
+            q = 1e-6
+
+        rms = (rms / q)**0.5
+        # compute rates
+        tpr = tp / q
+        tnr = tn / q
+        fpr = fp / q
+        fnr = fn / q
+        er = e / q
+        lr = l / q
+
+        # compute rate percentages
+        tprp = round(tpr * 100, 3)
+        tnrp = round(tnr * 100, 3)
+        fprp = round(fpr * 100, 3)
+        fnrp = round(fnr * 100, 3)
+        erp = round(er * 100, 3)
+        lrp = round(lr * 100, 3)
+        rmsp = round(rms * 100, 3)
+
+        c = np.corrcoef(actual_list, predicted_list)[0, 1]
+        cedb = round(10 * math.log(1-c, 10), 3) # correlation error, decibels
+
+        return {'rms':rms,'tp':tp,'tn':tn,'fp':fp,'fn':fn,'e':e,'l':l,'q':q,
+                'tpr':tpr,'tnr':tnr,'fpr':fpr,'fnr':fnr,'er':er,'lr':lr,
+                'tprp':tprp,'tnrp':tnrp,'fprp':fprp,'fnrp':fnrp,'erp':erp,'lrp':lrp, 'rmsp':rmsp,
+                'c':c,'cedb':cedb}
+
+    def graph_one_one(self, xs, ys, out_path):
+        '''
+        Generates and writes a logarithmic scatter plot centered on 1,1 to file.
+        Returns result of calculate_one_one_error.
+        '''
+
+        x_vals = [] # real values
+        y_vals = [] # predicted values
+
+        predicted_y = self.run_multi(xs)
+        # extract values from arrays
+        # TODO: make this less horribly inefficient
+        for outer_i in range(len(predicted_y)):
+            for inner_i in range(len(predicted_y[outer_i])):
+                x_vals.append(xs[outer_i][inner_i])
+                y_vals.append(predicted_y[outer_i][inner_i])
+
+        plt.close()
+
+        alpha = max(0.1, min(1, 10/(len(x_vals)+1)))
+        plt.plot(x_vals, y_vals, 'ro', alpha=alpha, linewidth=0.3, markersize=3)
+        plt.plot([.5,2],[.5,2], 'b')
+        plt.plot([1,1],[0.95,1.05],'b')
+        plt.plot([0.95,1.05],[1,1],'b')
+        plt.yscale('log')
+        plt.xscale('log')
+        plt.grid(True)
+        #plt.axis([0,2,0,2])
+        plt.xlabel('Real')
+        plt.ylabel('Predicted')
+
+        e = None
+
+        if labels:
+
+            e = calculate_error(x_vals, y_vals)
+            print(e)
+
+            lh = 3.1622777 # logarithmic half, 10^(1/2)
+
+            plt.text(lh, lh, 'True Positive:\n{}%'.format(e['tprp']), ha='center')
+            plt.text(1/lh, lh, 'False Positive:\n{}%'.format(e['fprp']), ha='center')
+            plt.text(lh, 1/lh, 'False Negative:\n{}%'.format(e['fnrp']), ha='center')
+            plt.text(1/lh, 1/lh, 'True Negative:\n{}%'.format(e['tnrp']), ha='center')
+
+            plt.text(1, 7, 'Overestimate: {}%'.format(e['erp']), ha='center')
+            plt.text(1, 0.13, 'Underestimate: {}%'.format(e['lrp']), ha='center')
+
+            title += '\nRMS={}% CatErr={}% cedB={}'.format(e['rmsp'],round(e['fprp']+e['fnrp'],3),e['cedb'])
+
+        plt.title(title)
+
+        plt.savefig(out_path, dpi=300)
+
+        return e
+
+class InvalidInputDimensions(Exception):
+    pass
---
+++ tests.py
@@ -0,0 +1,92 @@
+import unittest
+from main import *
+
+class ReferenceModelReturnsInput(SingleOutputModel):
+    def eval(self, x):
+        return x
+
+class ReferenceModelReturnsOne(SingleOutputModel):
+    def eval(self, x):
+        return 1
+
+class ReferenceModel1dOnly(SingleOutputModel):
+    def __init__(self):
+        self.x_shape = (-1, )
+
+    def eval(self, x):
+        return 1
+
+class ReferenceModelFixedShape(SingleOutputModel):
+    def __init__(self):
+        self.x_shape = (2,2)
+
+    def eval(self, x):
+        return 1
+
+class TestReferenceModels(unittest.TestCase):
+    def test_returns_input(self):
+        ri = ReferenceModelReturnsInput()
+        self.assertEqual(
+            [ri.run(x) for x in range(-20,20)],
+            list(range(-20,20))
+        )
+
+    def test_returns_1(self):
+        r1 = ReferenceModelReturnsOne()
+        self.assertEqual(
+            [r1.run(x) for x in range(-20,20)],
+            [1]*40
+        )
+
+    def test_1d_only(self):
+        rm = ReferenceModel1dOnly()
+        pass1 = True
+        try:
+            rm.run([1,2,3])
+        except InvalidInputDimensions:
+            pass1 = False
+        self.assertTrue(pass1)
+
+        pass2 = False
+        try:
+            rm.run([[3,4,2],[1,2,3]])
+        except InvalidInputDimensions:
+            pass2 = True
+        self.assertTrue(pass2)
+
+        pass3 = False
+        try:
+            rm.run(1)
+        except InvalidInputDimensions:
+            pass3 = True
+        self.assertTrue(pass3)
+
+    def test_fixed_shape(self):
+        rm = ReferenceModelFixedShape()
+        pass1 = True
+        try:
+            rm.run([[1,2],[3,4]])
+        except InvalidInputDimensions:
+            pass1 = False
+        self.assertTrue(pass1)
+
+        pass1 = False
+        try:
+            rm.run([[1,2],[3,4],[5,6]])
+        except InvalidInputDimensions:
+            pass1 = True
+        self.assertTrue(pass1)
+
+    def test_calculate_error(self):
+        rm = ReferenceModelReturnsOne()
+        o = rm.calculate_one_one_error([1,2,3],[0,1,1])
+        self.assertEqual(o,
+        {'c': 0.8660254037844385, 'cedb': -8.73, 'e': 0, 'er': 0.0,
+         'erp': 0.0, 'fn': 1, 'fnr': 0.3333333333333333, 'fnrp': 33.333,
+         'fp': 0, 'fpr': 0.0, 'fprp': 0.0, 'l': 3, 'lr': 1.0, 'lrp': 100.0,
+         'q': 3, 'rms': 1.4142135623730951, 'rmsp': 141.421, 'tn': 0,
+         'tnr': 0.0, 'tnrp': 0.0, 'tp': 2, 'tpr': 0.6666666666666666, 'tprp': 66.667})
+
+
+if __name__ == '__main__':
+    unittest.main()
