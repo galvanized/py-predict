071b08d915e44f67fa294efc18d9de7c60bde1ba
Shane Galvin <sgalvinnc@gmail.com>
2018-01-29 00:59:20 -0500

    now with basic exponential forecaster

--- analysis.py
+++ analysis.py
@@ -1,5 +1,6 @@
 from math import *
 from statistics import *
+from scipy.stats import norm
 
 def is_iter(v):
     # returns a boolean of whether v is iterable
@@ -9,6 +10,11 @@ def is_iter(v):
         return False
     else:
         return True
+
+def exponential_factor(initial, final, delta):
+    if initial == final or delta == 0:
+        return 1
+    return log(final/initial) / delta
 
 '''
     VECTOR FUNCTIONS
@@ -32,6 +38,15 @@ def v_multiply(v, m):
 
 def v_diff(v1, v2):
     return [i2 - i1 for i1,i2 in zip(v1, v2)]
+
+def vs_diff(v, vs):
+    # like v_diff but supporting both vector and scalar
+    # note that the subtraction order is reversed
+    if is_iter(vs):
+        return v_diff(vs, v)
+    else:
+        return [val - vs for val in v]
+
 
 def v_adiff(v1, v2):
     # absolute difference
@@ -114,3 +129,55 @@ def summa(v, c=0):
     for val in v:
         out.append(out[-1]+val)
     return out
+
+
+'''
+    SCALAR FUNCTIONS
+
+    The below functions each return a scalar value
+
+'''
+
+
+def rms(v, w=None):
+    # root mean square
+    # todo: branch if weights not needed
+    if w == None:
+        w = [1]*len(v)
+    cumulative_w = 0
+    cumulative_v = 0
+    for iv, iw in zip(v,w):
+        cumulative_w += iw
+        cumulative_v += iv**2 * iw
+    return(sqrt(cumulative_v/cumulative_w))
+
+def rmse(v, vs=0, w=None):
+    # root mean square error
+    # provide target as vs (can be vector or scalar)
+    # w is weights for v
+    return rms(vs_diff(v, vs), w)
+
+def mae(v, vs=0, f=mean):
+    # mean absolute error
+    return f([abs(a) for a in vs_diff(v, vs)])
+
+def exp_rate(v):
+    return [exponential_factor(val, v[-1], len(v)-i-1) for i, val in enumerate(v)]
+
+def twee(v, d, stdev=None, loc=None):
+    # time-weighted exponential extrapolation
+
+    # v: input vector
+    # d: displacement of prediction from last input vector
+
+    if loc == None:
+        loc = len(v) - d
+    if stdev == None:
+        stdev = 2*d
+
+    weights = [norm.pdf(x,loc=loc,scale=stdev) for x in range(len(v)-1)] + [0]
+    efs = exp_rate(v)
+
+    expected = sum([w*e for w, e in zip(weights, efs)])/sum(weights)
+
+    return v[-1] * exp(expected * d)
